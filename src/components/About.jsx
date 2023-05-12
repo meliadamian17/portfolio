b08e81c2a1b89bd3152808a0a3c30a1feace1205
damian <damian.melia@mail.utoronto.ca>
2023-05-12 13:43:37 -0400

    simple changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -46,7 +46,7 @@ const About = () => {
     <motion.p 
     variants ={fadeIn("","",0.1,1)}
       className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]">
-      quick overview goes here
+      finish later
     </motion.p>
 
     <div className="mt-20 flex flex-wrap gap-10 justify-center">
